4b71c843637d687891fa5a4e8508e14239fa4027
luanfsouza <luanferreira7777@gmail.com>
2024-02-24 22:40:37 -0300

    .

--- src/main/java/br/com/luanferreira/tutorial_spring_graphql/TutorialSpringGraphqlApplication.java
+++ src/main/java/br/com/luanferreira/tutorial_spring_graphql/TutorialSpringGraphqlApplication.java
@@ -2,9 +2,8 @@ package br.com.luanferreira.tutorial_spring_graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
+ 
+ BootApplication
 public class TutorialSpringGraphqlApplication {
  
 	 public static void main(String[] args) {
